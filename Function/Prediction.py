248f66399e7facd52d13cc198c67c5900a97d5d1
AtulBinu2002 <110544240+AtulBinu2002@users.noreply.github.com>
2023-03-10 21:17:19 +0530

    -

--- Function/Prediction.py
+++ Function/Prediction.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
+import requests
+from Image_conv import load_and_prep_image
 
 def pred_and_plot(model, filename, class_names):
+  request = request.get("https://raw.githubusercontent.com/AtulBinu2002/Sort-Your-Plate-CCN-/main/Function/Image_conv.py")
+  with open("Image_conv.py", "wb") as f:
+    f.write(request.content)
   """
   Imports an image located at filename, makes a prediction on it with
   a trained model and plots the image with the predicted class as the title.
   """
+
   # Import the target image and preprocess it
   img = load_and_prep_image(filename)
 
@@ -17,4 +23,5 @@ def pred_and_plot(model, filename, class_names):
   # Plot the image and predicted class
   plt.imshow(img)
   plt.title(f"Prediction: {pred_class}")
-  plt.axis(False);
+  plt.axis(False)
+  plt.show()
